20dfe6a779b5d0ee3e22e11e3d1e35763fce5bb9
Luis Cabral <lcabral37@gmail.com>
2013-06-07 09:48:19 +0200

    add James audio

---
+++ play2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+import subprocess as s
+
+s.call("mpg321 -g 1600 audio/mp31.mp3",shell=True)
---
+++ play3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+import subprocess as s
+
+s.call("mpg321 -g 1600 audio/mp32.mp3",shell=True)
